7530162e580635a19f71c723240a202b446ad6f5
john87barker <john87barker@gmail.com>
2021-07-01 13:44:17 -0600

    3

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('stuff')
+let health = 100
+
+function slap() {
+  console.log('you got slapped')
+
+
+
+}
+function losingHealth() {
+
+  for (let i = 100; i >= 0; i--) {
+    health = health - i
+
+  }
+}
