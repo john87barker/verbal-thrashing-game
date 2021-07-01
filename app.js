d9b25a6d434a220317c9fe87fb0514a6a6b9c3aa
john87barker <john87barker@gmail.com>
2021-07-01 14:21:57 -0600

    6

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 let life = 100
+let name = ''
+let hits = 0
 
-function slap() {
+
+function hit() {
+  hits += 1
+
+}
+
+
+function silly() {
   life -= 1
+  hit()
+  update()
+}
+
+function honor() {
+  life -= 5
+  hit()
+  update()
+}
+function momma() {
+  life -= 10
+  hit()
   update()
 }
 
 function update() {
   document.getElementById('health').innerText = life
+  document.getElementById('hits').innerText = hits
 
 }
 
